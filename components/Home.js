a5ed7a050042b90bce0db6eff409f8988706f253
LeoKnox <LeoKnox@users.noreply.github.com>
2026-06-06 14:26:31 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,17 @@
-import {N5Kanji} from "./N5Kanji.js";
+import { N5Kanji } from "./N5Kanji.js";
 
 export default Home = () => {
-  return <><h1>Home</h1>
-  <h2>{N5Kanji[1].Kanji}</h2></>
+  return (
+    <>
+      <h1>Home</h1>
+      <div>
+        <h2>{N5Kanji[1].Kanji}</h2>
+        <h3>
+          {N5Kanji[1].Furigana}
+          {N5Kanji[1].Romaji}
+          {N5Kanji[1].Meaning}
+        </h3>
+      </div>
+    </>
+  );
 };
