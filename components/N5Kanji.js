2fbdfbf8843c92dd12c8a71830546a03c41aac74
LeoKnox <LeoKnox@users.noreply.github.com>
2026-06-06 14:18:38 -0700

    Create N5Kanji.js

---
+++ components/N5Kanji.js
@@ -0,0 +1,15 @@
+
+export const N5Kanji = [
+  {
+    Kanji: "会う",
+    Furigana: "あう",
+    Romaji: "au",
+    Meaning: "to meet",
+  },
+  {
+    Kanji: "青い",
+    Furigana: "あおい",
+    Romaji: "aoi",
+    Meaning: "blue",
+  },
+];
