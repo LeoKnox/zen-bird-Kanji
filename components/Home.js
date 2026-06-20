c3d8600ff854dbfc77db13e359585300ad0728fb
LeoKnox <LeoKnox@users.noreply.github.com>
2026-06-20 11:24:31 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -37,169 +37,64 @@ export default Home = () => {
         >
           {hoverKanji >= 0 && <OnHover title={hoverKanji} />}?
         </p>
-        <table>
-          <tbody>
-            {N5Kanji.map((row, kanjiId) => (
-              <td
-                id={kanjiId}
-                onMouseEnter={() => setHoverKanji(kanjiId)}
-                onMouseLeave={() => setHoverKanji(null)}
+        <div className="kanjiRow">
+          {N5Kanji.map((row, kanjiId) => (
+            <div
+              id={kanjiId}
+              onMouseEnter={() => setHoverKanji(kanjiId)}
+              onMouseLeave={() => setHoverKanji(null)}
+            >
+              <p>
+                <input
+                  type="checkbox"
+                  value={kanjiId}
+                  onClick={(e) => updateKanjiList(e)}
+                />
+                {kanjiId}
+              </p>
+              {row.Kanji}
+              <div
+                style={{
+                  position: "absolute",
+                  display: "flex",
+                  verticalAlign: "top",
+                  top: "5em",
+                  left: "8em",
+                  backgroundColor: "gray",
+                  opacity: "80%",
+                  Padding: "10px",
+                  broderRadius: "10px",
+                }}
               >
-                <p>
-                  <input
-                    type="checkbox"
-                    value={kanjiId}
-                    onClick={(e) => updateKanjiList(e)}
-                  />
-                  {kanjiId}
-                </p>
-                {row.Kanji}
-                <div
-                  style={{
-                    position: "absolute",
-                    display: "flex",
-                    verticalAlign: "top",
-                    top: "5em",
-                    left: "8em",
-                    backgroundColor: "gray",
-                    opacity: "80%",
-                    Padding: "10px",
-                    broderRadius: "10px",
-                  }}
-                >
-                  {hoverKanji == kanjiId && (
-                    <>
-                      {Object.entries(row).map((data) => (
-                        <div
-                          name="info"
-                          style={{ overflow: "visible", display: "flex" }}
+                {hoverKanji == kanjiId && (
+                  <>
+                    {Object.entries(row).map((data) => (
+                      <div
+                        name="info"
+                        style={{ overflow: "visible", display: "flex" }}
+                      >
+                        <label
+                          key={data}
+                          style={{
+                            padding: "8px",
+                            textAlign: "left",
+                            fontWeight: "bold",
+                            fontSize: "1em",
+                            overflow: "visible",
+                          }}
                         >
-                          <label
-                            key={data}
-                            style={{
-                              padding: "8px",
-                              textAlign: "left",
-                              fontWeight: "bold",
-                              fontSize: "1em",
-                              overflow: "visible",
-                            }}
-                          >
-                            {hoverKanji >= 0 && (
-                              <OnHover title={data[0]} data={data[1]} />
-                            )}
-                          </label>
-                        </div>
-                      ))}
-                    </>
-                  )}
-                </div>
-              </td>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
-};
-import { N5Kanji } from "./N5Kanji.js";
-import { useState } from "react";
-
-export default Home = () => {
-  const [kanjiList, setKanjiList] = useState([]);
-  const [hoverKanji, setHoverKanji] = useState(null);
-  const updateKanjiList = (e) => {
-    e.target.checked
-      ? setKanjiList([...kanjiList, e.target.value])
-      : setKanjiList((kanji) =>
-          kanji.filter((index) => e.target.value !== index)
-        );
-  };
-  const OnHover = (props) => {
-    //setHoverKanji(props.kanjiId);
-    return (
-      <div
-        style={{
-          zindex: 10,
-        }}
-      >
-        <label>
-          {props.title}:{props.data}
-        </label>
-      </div>
-    );
-  };
-  return (
-    <>
-      <h1>Home</h1>
-      <div>
-        <h2>家</h2>
-        <p
-          style={{
-            height: "1em",
-          }}
-        >
-          {hoverKanji >= 0 && <OnHover title={hoverKanji} />}?
-        </p>
-        <table>
-          <tbody>
-            {N5Kanji.map((row, kanjiId) => (
-              <td
-                id={kanjiId}
-                onMouseEnter={() => setHoverKanji(kanjiId)}
-                onMouseLeave={() => setHoverKanji(null)}
-              >
-                <p>
-                  <input
-                    type="checkbox"
-                    value={kanjiId}
-                    onClick={(e) => updateKanjiList(e)}
-                  />
-                  {kanjiId}
-                </p>
-                {row.Kanji}
-                <div
-                  style={{
-                    position: "absolute",
-                    display: "flex",
-                    verticalAlign: "top",
-                    top: "5em",
-                    left: "8em",
-                    backgroundColor: "gray",
-                    opacity: "80%",
-                    Padding: "10px",
-                    broderRadius: "10px",
-                  }}
-                >
-                  {hoverKanji == kanjiId && (
-                    <>
-                      {Object.entries(row).map((data) => (
-                        <div
-                          name="info"
-                          style={{ overflow: "visible", display: "flex" }}
-                        >
-                          <label
-                            key={data}
-                            style={{
-                              padding: "8px",
-                              textAlign: "left",
-                              fontWeight: "bold",
-                              fontSize: "1em",
-                              overflow: "visible",
-                            }}
-                          >
-                            {hoverKanji >= 0 && (
-                              <OnHover title={data[0]} data={data[1]} />
-                            )}
-                          </label>
-                        </div>
-                      ))}
-                    </>
-                  )}
-                </div>
-              </td>
-            ))}
-          </tbody>
-        </table>
+                          {hoverKanji >= 0 && (
+                            <OnHover title={data[0]} data={data[1]} />
+                          )}
+                        </label>
+                      </div>
+                    ))}
+                  </>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
