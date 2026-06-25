b93bbe907c35a75a820ddc257732b91772e4d3a0
LeoKnox <LeoKnox@users.noreply.github.com>
2026-06-25 12:56:34 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -37,83 +37,85 @@ export default Home = () => {
         >
           {hoverKanji >= 0 && <OnHover title={hoverKanji} />}
         </p>
-        <div
-          className="kanjiRow"
-          style={{ overflow: "visible", display: "flex" }}
-        >
-          {N5Kanji.map((row, kanjiId) => (
-            <div
-              className="kanjiColumn"
-              id={kanjiId}
-              onMouseEnter={() => setHoverKanji(kanjiId)}
-              onMouseLeave={() => setHoverKanji(null)}
-            >
-              <p>
-                <input
-                  type="checkbox"
-                  value={kanjiId}
-                  onClick={(e) => updateKanjiList(e)}
-                />
-                {kanjiId}
-              </p>
-              {row.Kanji}
+        <div className="kanjiWrapper" style={{ display: "flex" }}>
+          <div
+            className="kanjiRow"
+            style={{ overflow: "visible", display: "flex" }}
+          >
+            {N5Kanji.map((row, kanjiId) => (
               <div
-                style={{
-                  position: "absolute",
-                  display: "flex",
-                  verticalAlign: "top",
-                  top: "5em",
-                  left: "8em",
-                  backgroundColor: "gray",
-                  opacity: "80%",
-                  Padding: "10px",
-                  broderRadius: "10px",
-                  alignItems: "flex-start",
-                }}
+                className="kanjiColumn"
+                id={kanjiId}
+                onMouseEnter={() => setHoverKanji(kanjiId)}
+                onMouseLeave={() => setHoverKanji(null)}
               >
-                {hoverKanji == kanjiId && (
-                  <>
-                    {Object.entries(row).map((data) => (
-                      <div name="info">
-                        <label
-                          key={data}
-                          style={{
-                            display: "block",
-                            padding: "8px",
-                            textAlign: "left",
-                            fontWeight: "bold",
-                            fontSize: "1em",
-                            overflow: "visible",
-                          }}
-                        >
-                          {hoverKanji >= 0 && (
-                            <OnHover title={data[0]} data={data[1]} />
-                          )}
-                        </label>
-                      </div>
-                    ))}
-                  </>
-                )}
+                <p>
+                  <input
+                    type="checkbox"
+                    value={kanjiId}
+                    onClick={(e) => updateKanjiList(e)}
+                  />
+                  {kanjiId}
+                </p>
+                {row.Kanji}
+                <div
+                  style={{
+                    position: "absolute",
+                    display: "flex",
+                    verticalAlign: "top",
+                    top: "5em",
+                    left: "8em",
+                    backgroundColor: "gray",
+                    opacity: "80%",
+                    Padding: "10px",
+                    broderRadius: "10px",
+                    alignItems: "flex-start",
+                  }}
+                >
+                  {hoverKanji == kanjiId && (
+                    <>
+                      {Object.entries(row).map((data) => (
+                        <div name="info">
+                          <label
+                            key={data}
+                            style={{
+                              display: "block",
+                              padding: "8px",
+                              textAlign: "left",
+                              fontWeight: "bold",
+                              fontSize: "1em",
+                              overflow: "visible",
+                            }}
+                          >
+                            {hoverKanji >= 0 && (
+                              <OnHover title={data[0]} data={data[1]} />
+                            )}
+                          </label>
+                        </div>
+                      ))}
+                    </>
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-        <div
-          className="sideBar"
-          style={{
-            display: "grid",
-            alignContent: "start",
-          }}
-        >
-          <p>Kanji</p>
-          <label
+            ))}
+          </div>
+          <div
+            className="sideBar"
             style={{
-              writingMode: "vertical-rl",
-              textOrientation: "upright",
+              display: "grid",
+              alignContent: "start",
             }}
           >
-            {kanjiList}
-          </label>
+            <p>Kanji</p>
+            <label
+              style={{
+                writingMode: "vertical-rl",
+                textOrientation: "upright",
+              }}
+            >
+              {kanjiList}
+            </label>
+          </div>
         </div>
       </div>
     </>
