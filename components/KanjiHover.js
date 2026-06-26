47d9eff49daefd19dd6cddd68706add85b037030
LeoKnox <LeoKnox@users.noreply.github.com>
2026-06-26 15:03:01 -0700

    Add KanjiHover component for displaying kanji info

---
+++ components/KanjiHover.js
@@ -0,0 +1,23 @@
+export default KanjiHover = () => {
+  return (
+    <>
+      {Object.entries(row).map((data) => (
+        <div name="info">
+          <label
+            key={data}
+            style={{
+              display: "block",
+              padding: "8px",
+              textAlign: "left",
+              fontWeight: "bold",
+              fontSize: "1em",
+              overflow: "visible",
+            }}
+          >
+            {hoverKanji >= 0 && <OnHover title={data[0]} data={data[1]} />}
+          </label>
+        </div>
+      ))}
+    </>
+  );
+};
