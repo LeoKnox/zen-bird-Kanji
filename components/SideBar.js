9b2d23e7256a61f671439459f3d84d18381f5d2d
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-02 16:11:18 -0700

    Add SideBar component for displaying Kanji list

---
+++ components/SideBar.js
@@ -0,0 +1,35 @@
+export default SideBar = ({ kanjiList }) => {
+  return (
+    <div
+      className="sideBar"
+      style={{
+        display: "grid",
+        alignContent: "start",
+        flex: "0 0 0 1",
+        backgroundColor: "#506b3c",
+        color: "#88a564",
+        minWidth: "auto",
+        padding: "10px",
+        minHeight: "60vh",
+        position: "relative",
+      }}
+    >
+      <p>Kanji</p>
+      <label
+        style={{
+          writingMode: "vertical-rl",
+          textOrientation: "upright",
+          backgroundColor: "#ccff99",
+          padding: "5px", // move to css file to clear empty
+          borderRadius: "1em",
+        }}
+      >
+        {kanjiList}
+      </label>
+      <div style={{ position: "absolute", bottom: 0, left: 0, with: "100%" }}>
+        <button className="kanjiButton">practice</button>
+        <button className="kanjiButton">quiz</button>
+      </div>
+    </div>
+  );
+};
