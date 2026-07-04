1ab909d4abb23eaeceee3cba34179ee583610375
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-04 13:29:36 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -4,16 +4,6 @@ import SideBar from "./SideBar.js";
 import KanjiSelect from "./KanjiSelect.js";
 
 export default Home = () => {
-  const [kanjiList, setKanjiList] = useState([]);
-  const [hoverKanji, setHoverKanji] = useState(null);
-  const updateKanjiList = (e) => {
-    e.target.checked
-      ? setKanjiList([...kanjiList, e.target.value])
-      : setKanjiList((kanji) =>
-          kanji.filter((index) => e.target.value !== index)
-        );
-  };
-
   return (
     <>
       <h1>Home</h1>
