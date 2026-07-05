916e9a040b75e155e49e124076327f804f98dd3f
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-05 12:06:02 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -4,13 +4,14 @@ import SideBar from "./SideBar.js";
 import KanjiSelect from "./KanjiSelect.js";
 
 export default Home = () => {
+  const  [currentPage, setCurrentPage] = useState("kanjiSelect");
   const availablePages = { kanjiSelect: <KanjiSelect N5Kanji={N5Kanji} /> };
   return (
     <>
       <h1>Home</h1>
       <div>
         <h2>家</h2>
-        {availablePages["kanjiSelect"]}
+        {availablePages[currentPage]}
       </div>
     </>
   );
