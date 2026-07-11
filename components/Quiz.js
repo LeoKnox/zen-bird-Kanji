76b5716000fea426e9054c6f9a9cb1bb6903f4d3
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-11 13:03:27 -0700

    Update Quiz.js

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,15 +1,17 @@
+import { singleKanji } from "./N5Kanji.js";
+
 export default Quiz = ({ setCurrentPage, kanjiList }) => {
   return (
     <>
       <p>クイズ</p>
       <button className="kanjiButton" onClick={() => setCurrentPage("家")}>
-        Home
         <div className="kanjiQuiz">
           {kanjiList.map((kanji) => (
             <label className="quizBox">+{kanji}</label>
           ))}
         </div>
       </button>
+      {() => singleKanji()}
     </>
   );
 };
