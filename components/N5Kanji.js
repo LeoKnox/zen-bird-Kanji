b2c969e27bfbcb900ba048961403de0fef50cc4c
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-11 17:06:09 -0700

    Add additional Kanji entries to N5Kanji

--- components/N5Kanji.js
+++ components/N5Kanji.js
@@ -8,7 +8,49 @@ export const N5Kanji = [
     Meaning: "to meet",
   },
   {
+    Kanji: "青い",// https://www.llanai.com/jlpt/n5/vocabulary/complete
+export const singleKanji = (index = 1) =>
+  index >= 0 && index < N5Kanji.length ? N5Kanji[index].meaning : null;
+
+export const N5Kanji = [
+  {
+    Kanji: "会う",
+    Furigana: "あう",
+    Romaji: "au",
+    Meaning: "to meet",
+  },
+  {
     Kanji: "青い",
+    Furigana: "あおい",
+    Romaji: "aoi",
+    Meaning: "blue",
+  },
+  {
+    Kanji: "北",
+    Furigana: "きた",
+    Romaji: "kita",
+    Meaning: "north",
+  },
+  {
+    Kanji: "南",
+    Furigana: "みなみ",
+    Romaji: "minami",
+    Meaning: "south",
+  },
+  {
+    Kanji: "西",
+    Furigana: "にし",
+    Romaji: "nishi",
+    Meaning: "west",
+  },
+  {
+    Kanji: "東",
+    Furigana: "ひがし",
+    Romaji: "higashi",
+    Meaning: "east",
+  },
+];
+
     Furigana: "あおい",
     Romaji: "aoi",
     Meaning: "blue",
