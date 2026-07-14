3c07488d67dc1ee005d58703345d208c053e4c4c
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-14 12:41:40 -0700

    Update Quiz.js

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,6 +1,7 @@
 import { singleKanji } from "./N5Kanji.js";
 
 export default Quiz = ({ setCurrentPage, kanjiList }) => {
+  let kanjiQuiz = [...kanjiList].sort(() => 0.5 - Math.random()).slice(0, 6);
   console.log(singleKanji());
   return (
     <>
@@ -8,12 +9,9 @@ export default Quiz = ({ setCurrentPage, kanjiList }) => {
 
       <div className="kanjiQuiz">
         {kanjiList.length > 5 ? (
-          [...kanjiList]
-            .sort(() => 0.5 - Math.random())
-            .slice(0, 6)
-            .map((kanji) => (
-              <label className="quizBox">{singleKanji(kanji).Kanji}</label>
-            ))
+          kanjiQuiz.map((kanji) => (
+            <label className="quizBox">{singleKanji(kanji).Kanji}</label>
+          ))
         ) : (
           <p>Please select 6 Kanji</p>
         )}
