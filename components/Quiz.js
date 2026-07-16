f53885e2829f9c2487a0c54e6bd9df4096aeb604
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-16 11:55:21 -0700

    Update Quiz.js

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,1 +1,33 @@
-Math.floor(Math.random() * 7)
+import { singleKanji } from "./N5Kanji.js";
+import { useState } from "react";
+
+export default Quiz = ({ setCurrentPage, kanjiList }) => {
+  const [kanjiQuiz, setKanjiQuiz] = useState(
+    [...kanjiList].sort(() => 0.5 - Math.random()).slice(0, 6)
+  );
+  const [kanjiAnswer, setKanjiAnswer] = useState(Math.floor(Math.random() * 7));
+  console.log(singleKanji());
+  const updateKanjiQuiz = () => {
+    setKanjiQuiz([...kanjiList].sort(() => 0.5 - Math.random()).slice(0, 6));
+    setKanjiAnswer(Math.floor(Math.random() * 7));
+  };
+  return (
+    <>
+      <p>クイズ</p>+{[kanjiAnswer]}+
+      <div className="kanjiQuiz">
+        {kanjiList.length > 5 ? (
+          kanjiQuiz.map((kanji) => (
+            <label onClick={() => updateKanjiQuiz()} className="quizBox">
+              {singleKanji(kanji).Kanji}
+            </label>
+          ))
+        ) : (
+          <p>Please select 6 Kanji</p>
+        )}
+      </div>
+      <button className="kanjiButton" onClick={() => setCurrentPage(["家"])}>
+        Home
+      </button>
+    </>
+  );
+};
