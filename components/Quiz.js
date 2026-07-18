e7ed43b599e850cb5b1012555d1d51e5af7a17d4
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-17 18:27:34 -0700

    Modify updateKanjiQuiz to include event check
    Add event parameter check in updateKanjiQuiz function

--- components/Quiz.js
+++ components/Quiz.js
@@ -7,9 +7,11 @@ export default Quiz = ({ setCurrentPage, kanjiList }) => {
   );
   const [kanjiAnswer, setKanjiAnswer] = useState(Math.floor(Math.random() * 7));
   console.log(singleKanji());
-  const updateKanjiQuiz = () => {
-    setKanjiQuiz([...kanjiList].sort(() => 0.5 - Math.random()).slice(0, 6));
-    setKanjiAnswer(Math.floor(Math.random() * 6));
+  const updateKanjiQuiz = (e) => {
+    if (e.target.id >= 0) {
+      setKanjiQuiz([...kanjiList].sort(() => 0.5 - Math.random()).slice(0, 6));
+      setKanjiAnswer(Math.floor(Math.random() * 6));
+    }
   };
   return (
     <>
