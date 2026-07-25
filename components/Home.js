723228112ad4746d4d06fa01a39bd57df173b5da
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-25 12:39:31 -0700

    Initialize kanjiList with numbers 0-9

--- components/Home.js
+++ components/Home.js
@@ -2,11 +2,12 @@ import { N5Kanji } from "./N5Kanji.js";
 import { useState } from "react";
 import SideBar from "./SideBar.js";
 import KanjiSelect from "./KanjiSelect.js";
+import Practice from "./Practice.js";
 import Quiz from "./Quiz.js";
 
 export default Home = () => {
   const [currentPage, setCurrentPage] = useState("家");
-  const [kanjiList, setKanjiList] = useState([]);
+  const [kanjiList, setKanjiList] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
   const availablePages = {
     家: (
       <KanjiSelect
@@ -16,6 +17,7 @@ export default Home = () => {
         kanjiList={kanjiList}
       />
     ),
+    練習: <Practice setCurrentPage={setCurrentPage} kanjiList={kanjiList} />,
     クイズ: <Quiz setCurrentPage={setCurrentPage} kanjiList={kanjiList} />,
   };
   return (
