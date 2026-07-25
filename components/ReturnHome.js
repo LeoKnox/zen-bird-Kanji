a155911079535589c6b46daca0280184f104f9c1
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-25 14:17:43 -0700

    Create ReturnHome.js

---
+++ components/ReturnHome.js
@@ -0,0 +1,10 @@
+export default ReturnHome = () => {
+  return (
+    <>
+      +
+      <button className="kanjiButton" onClick={() => setCurrentPage(["家"])}>
+        Home
+      </button>
+    </>
+  );
+};
