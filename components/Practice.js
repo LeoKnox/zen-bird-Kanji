37dbf81031128eddf3d2b1caf275cf118a94dc0e
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-26 14:16:30 -0700

    Update Practice.js

--- components/Practice.js
+++ components/Practice.js
@@ -2,9 +2,9 @@ import ReturnHome from "./ReturnHome.js";
 
 export default Practice = () => {
   return (
-    <>
+    <div>
       <h3>Practice</h3>
       <ReturnHome />
-    </>
+    </div>
   );
 };
