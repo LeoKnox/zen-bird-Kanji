d11ba8f88ecf341b9ec2f028769d049c4d055296
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-28 15:38:37 -0700

    Update Practice.js

--- components/Practice.js
+++ components/Practice.js
@@ -1,13 +1,17 @@
 import { singleKanji } from "./N5Kanji.js";
+import { useState } from "react";
 import ReturnHome from "./ReturnHome.js";
 
 export default Practice = ({ kanjiList }) => {
+  const [target, setTarget] = useState(
+    singleKanji(Math.floor(Math.random() * kanjiList.length))
+  );
   return (
     <>
       <button>-</button>
       <button>Random</button>
       <button>+</button>
-      <p>Target:</p>
+      <p>Target:{target.Furigana}</p>
       <div>
         {kanjiList.map((v, i) => (
           <p>{singleKanji(i).Furigana}</p>
