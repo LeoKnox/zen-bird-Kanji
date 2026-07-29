159adc5bc068744259d2aa42490437ab5a684f09
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-29 15:04:15 -0700

    Update SideBar.js

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,31 +1,6 @@
 export default SideBar = ({ kanjiList, setCurrentPage }) => {
   return (
-    <div
-      className="sideBar"
-      style={{
-        display: "grid",
-        alignContent: "start",
-        flex: "0 0 0 1",
-        backgroundColor: "#506b3c",
-        color: "#88a564",
-        minWidth: "auto",
-        padding: "10px",
-        minHeight: "60vh",
-        position: "absolute",
-      }}
-    >
-      <p>Kanji</p>
-      <label
-        style={{
-          writingMode: "vertical-rl",
-          textOrientation: "upright",
-          backgroundColor: "#ccff99",
-          padding: "5px", // move to css file to clear empty
-          borderRadius: "1em",
-        }}
-      >
-        {kanjiList}
-      </label>
+    <>
       <div style={{ position: "absolute", bottom: 0, left: 0, with: "100%" }}>
         <button className="kanjiButton" onClick={() => setCurrentPage("練習")}>
           practice
@@ -37,6 +12,33 @@ export default SideBar = ({ kanjiList, setCurrentPage }) => {
           quiz
         </button>
       </div>
-    </div>
+      <div
+        className="sideBar"
+        style={{
+          display: "grid",
+          alignContent: "start",
+          flex: "0 0 0 1",
+          backgroundColor: "#506b3c",
+          color: "#88a564",
+          minWidth: "auto",
+          padding: "10px",
+          minHeight: "60vh",
+          position: "relative",
+        }}
+      >
+        <p>Kanji</p>
+        <label
+          style={{
+            writingMode: "vertical-rl",
+            textOrientation: "upright",
+            backgroundColor: "#ccff99",
+            padding: "5px", // move to css file to clear empty
+            borderRadius: "1em",
+          }}
+        >
+          {kanjiList}
+        </label>
+      </div>
+    </>
   );
 };
