6a2f6cee23741b6db32a6b1dfe37a961d7b06669
LeoKnox <LeoKnox@users.noreply.github.com>
2026-07-31 14:36:07 -0700

    Update SideBar.js

--- components/SideBar.js
+++ components/SideBar.js
@@ -30,7 +30,7 @@ export default SideBar = ({ kanjiList, setCurrentPage }) => {
           {kanjiList}
         </label>
         <div
-          style={{ backgroundColor: "tan", padding: "5px", margin: "-10px" }}
+          style={{ backgroundColor: "tan", padding: "5px", margin: "-8px" }}
         >
           <button
             className="kanjiButton"
