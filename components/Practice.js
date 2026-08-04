1b71e24b23830b467284c0cf24ef072425a42a5d
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-04 13:13:10 -0700

    Update Practice.js

--- components/Practice.js
+++ components/Practice.js
@@ -39,12 +39,12 @@ export default Practice = ({ kanjiList }) => {
             style={{
               flex: "0 0 calc((100% - 12px * 4) / 5)",
               whiteSpace: "nowrap",
-              backgroundColor: target == i ? "red" : "green",
+              backgroundColor: target.Kanji == v ? "red" : "green",
             }}
           >
             {singleKanji(i).Furigana}
             <p>
-              {target}:{i}
+              {target.Kanji}:{v}
             </p>
           </label>
         ))}
