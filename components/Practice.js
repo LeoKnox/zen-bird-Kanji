9629cc30e7106c91605d25b2077700f3616bcb73
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-05 18:43:48 -0700

    Update Practice.js

--- components/Practice.js
+++ components/Practice.js
@@ -4,9 +4,9 @@ import ReturnHome from "./ReturnHome.js";
 
 export default Practice = ({ kanjiList }) => {
   const [active, setActive] = useState(0);
-  const [target, setTarget] = useState(
-    singleKanji(Math.floor(Math.random() * kanjiList.length))
-  );
+  const [target, setTarget] = useState([
+    singleKanji(Math.floor(Math.random() * kanjiList.length)),
+  ]);
   return (
     <>
       <button>-</button>
@@ -20,7 +20,7 @@ export default Practice = ({ kanjiList }) => {
         Random
       </button>
       <button>+</button>
-      <p>Target:{target.Furigana}</p>
+      <p>Target:{target[0].Furigana}</p>
       <div
         style={{
           display: "flex",
@@ -40,13 +40,11 @@ export default Practice = ({ kanjiList }) => {
               flex: "0 0 calc((100% - 12px * 4) / 5)",
               whiteSpace: "nowrap",
               backgroundColor:
-                kanjiList[v].Kanji == target.Kanji ? "red" : "green",
+                kanjiList[v].Kanji == target[0].Kanji ? "red" : "green",
             }}
           >
             {singleKanji(i).Furigana}
-            <p>
-              {kanjiList[v]}:{v}
-            </p>
+            <p>:{v}</p>
           </label>
         ))}
       </div>
