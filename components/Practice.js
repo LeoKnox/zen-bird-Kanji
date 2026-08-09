78c7dbd257fe295b49e5158efcd7562cd173977d
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-09 13:43:38 -0700

    Refactor target setting and clean up Practice.js
    Updated target setting to use singleKanji function and removed unused elements.

--- components/Practice.js
+++ components/Practice.js
@@ -24,7 +24,7 @@ export default Practice = ({ kanjiList }) => {
     let temp = Math.floor(Math.random() * kanjiList.length);
     clickTimeoutRef.current = setTimeout(() => {
       setFind(temp);
-      setTarget(temp);
+      setTarget(singleKanji(temp));
     }, 250);
   };
   return (
@@ -35,46 +35,6 @@ export default Practice = ({ kanjiList }) => {
         onDoubleClick={() => setActive(!active)}
         style={{
           color: "yellow",
-          backgroundColor: active ? "green" : "brown",
-        }}
-      >
-        Random
-      </button>
-      <button>+</button>
-      <p>
-        Target:{target.Furigana}:{find}
-      </p>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "nowrap",
-          overflowX: "auto",
-          overflowY: "hidden",
-          scrollSnapType: "x mandatory",
-          scrollbarWidth: "thin",
-          scrollbarColor: "green transparent",
-          //scrollbarWidth: "none",
-        }}
-      >
-        {kanjiList.map((v, i) => (
-          <label
-            className="kanjiPractice"
-            style={{
-              flex: "0 0 calc((100% - 12px * 4) / 5)",
-              whiteSpace: "nowrap",
-              backgroundColor: find == v ? "red" : "green",
-            }}
-          >
-            {singleKanji(i).Furigana}
-            <p>
-              {find}:{v}
-            </p>
-          </label>
-        ))}
-      </div>
-    </>
-  );
-};
           backgroundColor: active ? "green" : "brown",
         }}
       >
