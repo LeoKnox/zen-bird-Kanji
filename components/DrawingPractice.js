17d747773d561cb3e884bec9c11b4ff0f9c5d16b
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-14 16:25:44 -0700

    Update DrawingPractice component to include SVG

--- components/DrawingPractice.js
+++ components/DrawingPractice.js
@@ -1,3 +1,9 @@
 export default DrawingPractice = () => {
-  return <div className="drawingPractice">Draw here</div>;
+  return (
+    <div className="drawingPractice">
+      <svg>
+        <line x1="0" y1="80" x2="100" y2="20" stroke="blue" />
+      </svg>
+    </div>
+  );
 };
