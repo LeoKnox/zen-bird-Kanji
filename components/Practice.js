7729a493005199214cf60123c3976a35f968f85c
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-19 13:07:57 -0700

    Update Practice.js

--- components/Practice.js
+++ components/Practice.js
@@ -30,7 +30,7 @@ export default Practice = ({ kanjiList }) => {
         setFind(temp);
       }, 250);
       setPaths([]);
-    } else if (find < kanjiList.length) {
+    } else if (find > kanjiList.length) {
       setFind(0);
       setPaths([]);
     } else {
